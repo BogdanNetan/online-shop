e981e14d2e91217a76043256615025d514f4027d
BogdanNetan <netan.bogdan@gmail.com>
2020-02-19 19:26:13 +0200

    update

---
+++ src/main/java/org/fasttrackit/onlineshop/domain/Product.java
@@ -0,0 +1,81 @@
+package org.fasttrackit.onlineshop.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class Product {
+
+    // id se aplica doar a primul rand,nu la tot!
+    @Id
+    private long id;
+    @NotNull
+    private String name;
+    private String description;
+    @NotNull
+    private double price;
+    @NotNull
+    private int qantity;
+    private String imageUrl;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQantity() {
+        return qantity;
+    }
+
+    public void setQantity(int qantity) {
+        this.qantity = qantity;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                ", qantity=" + qantity +
+                ", imageUrl='" + imageUrl + '\'' +
+                '}';
+    }
+}
