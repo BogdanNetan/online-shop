736437419633548ed25cf80633abde1fb1dcced9
BogdanNetan <netan.bogdan@gmail.com>
2020-03-13 20:36:19 +0200

    added Product controller

--- src/main/java/org/fasttrackit/onlineshop/exception/ResourceNotFondException.java
+++ src/main/java/org/fasttrackit/onlineshop/exception/ResourceNotFondException.java
@@ -1,6 +1,12 @@
 package org.fasttrackit.onlineshop.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+
 public class ResourceNotFondException extends RuntimeException {
+
 
     public ResourceNotFondException(String message) {
         super(message);
